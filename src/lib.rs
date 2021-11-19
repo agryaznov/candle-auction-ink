f98189547025c6cf8390e024900426218956654b
Alexander Gryaznov <hi@agryaznov.com>
2021-11-19 17:29:05 +0300

    save: get_candle_winner() compiles, need tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ mod candle_auction {
     };
     use ink_storage::collections::HashMap as StorageHashMap;
     use ink_storage::Vec as StorageVec;
-    use scale::{Encode, Decode};
+    use scale::{Decode, Encode};
     // use parity_scale_codec::Decode
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
@@ -385,44 +385,44 @@ mod candle_auction {
             }
         }
 
-        /// Helper to get the Candle auction winner: 
+        /// Helper to get the Candle auction winner:
         ///  Get random block in Ending period,  
         ///  then get the highest bidder in that block
         /// 1. Easy lvl: use ink_env::random
-        /// TODO: 2. Intermediate lvl: use chain extension like in ink rand-extension example 
+        /// TODO: 2. Intermediate lvl: use chain extension like in ink rand-extension example
         /// TODO: this sould be invoked automatically? or not? maybe not, but once
-        pub fn get_candle_winner(&self) -> Option<(AccountId,Balance)> {
-            // TODO: we use final top bid amount as the input buffer to `random()` func   
+        pub fn get_candle_winner(&self) -> Option<(AccountId, Balance)> {
+            // TODO: we use final top bid amount as the input buffer to `random()` func
             // for additional hash randomization
             // let mut winning_balance = 0;
             // if let Some(winning) = self.winning {
             //     winning_balance = *self.balances.get(&winning).unwrap_or(&0);
-            // }    
-            if self.get_status() == Status::Ended { // TODO: 1. and candle winner hasn't defined yet 2. if there is no winner => there is no candle winner!
+            // }
+            if self.get_status() == Status::Ended {
+                // TODO: 1. and candle winner hasn't defined yet 2. if there is no winner => there is no candle winner!
                 let opening_period_last_block = self.start_block + self.opening_period - 1;
                 let ending_period_last_block = opening_period_last_block + self.ending_period;
-    
-                let (raw_offset, known_since): (Hash, BlockNumber) = ink_env::random(&b"candle_auction"[..])
-                                                    .expect("cannot get randomness!");
+
+                let (raw_offset, known_since): (Hash, BlockNumber) =
+                    ink_env::random::<Environment>(&b"candle_auction"[..])
+                        .expect("cannot get randomness!");
                 if ending_period_last_block < known_since {
-                    // (Inspired by: 
+                    // (Inspired by:
                     //   https://github.com/paritytech/polkadot/blob/v0.9.13-rc1/runtime/common/src/auctions.rs#L526)
                     // Our random seed was known only after the auction ended. Good to use.
-					let raw_offset_block_number = <BlockNumber>::decode(
-						&mut raw_offset.as_ref(),
-					)
-					.expect("secure hashes should always be bigger than the block number; qed");
+                    let raw_offset_block_number = <BlockNumber>::decode(&mut raw_offset.as_ref())
+                        .expect("secure hashes should always be bigger than the block number; qed");
 
                     // detect the block when 'the candle went out' in Ending Period
                     let offset = raw_offset_block_number % self.ending_period + 1;
 
                     // Detect winner.
-                    // Starting from `candle-detected` block,  
+                    // Starting from `candle-detected` block,
                     // iterate backwards until a block with bids found
-                    let mut win_data: Option<(AccountId,Balance)> = None;
-                    for data in self.winning_data.iter().take(usize::try_from(offset).unwrap()).rev() {
-                        if let Some(_) = data {
-                            win_data = data;   
+                    let mut win_data: Option<(AccountId, Balance)> = None;
+                    for i in (1..offset + 1).rev() {
+                        if let Some((w, b)) = self.winning_data.get(i).unwrap() {
+                            win_data = Some((*w, *b));
                             break;
                         }
                     }
